1a41f1ba0c78dcf54d08eee8fcb9497ad211d474
Ian Jauslin <ian.jauslin@rutgers.edu>
2024-01-24 13:01:18 -0500

    Fix document generation for blueprint

--- PrimeNumberTheoremAnd.lean
+++ PrimeNumberTheoremAnd.lean
@@ -1,3 +1,11 @@
 -- This module serves as the root of the `PrimeNumberTheoremAnd` library.
 -- Import modules here that should be built as part of the library.
-import «PrimeNumberTheoremAnd».Basic
+--import «PrimeNumberTheoremAnd».Basic
+
+import «PrimeNumberTheoremAnd».HadamardFactorization
+import «PrimeNumberTheoremAnd».HoffsteinLockhart
+import «PrimeNumberTheoremAnd».MellinCalculus
+import «PrimeNumberTheoremAnd».ResidueCalcOnRectangles
+import «PrimeNumberTheoremAnd».SecondProofPNT
+import «PrimeNumberTheoremAnd».StrongPNT
+import «PrimeNumberTheoremAnd».Wiener
