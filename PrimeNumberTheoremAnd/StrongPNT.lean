45bef9503c1558ec7fd4b0bed2a4afc5cb8fc417
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2024-01-29 07:58:42 -0500

    update

--- PrimeNumberTheoremAnd/StrongPNT.lean
+++ PrimeNumberTheoremAnd/StrongPNT.lean
@@ -1,35 +1,31 @@
-import Mathlib.Analysis.Complex.CauchyIntegral
 import Mathlib.NumberTheory.VonMangoldt
 import Mathlib.NumberTheory.ArithmeticFunction
-import Mathlib.NumberTheory.ZetaFunction
-import Mathlib.Analysis.Analytic.Meromorphic
-import PrimeNumberTheoremAnd.EulerProducts.LSeries
 
-open Complex BigOperators Finset Nat Classical
+open Nat
 
-open scoped ArithmeticFunction Interval
+open scoped ArithmeticFunction
 
 /-%%
-\begin{definition}\label{ChebyshevPsi}\lean{ChebyshevPsi}\leanok
+\begin{definition}%\label{ChebyshevPsi}\lean{ChebyshevPsi}\leanok
 The Chebyshev Psi function is defined as
 $$
-\psi(x) = \sum_{n \leq x} \Lambda(n),
+\psi(x) := \sum_{n \le x} \Lambda(n),
 $$
 where $\Lambda(n)$ is the von Mangoldt function.
 \end{definition}
 %%-/
 noncomputable def ChebyshevPsi (x : ℝ) : ℝ := (Finset.range (Nat.floor x)).sum Λ
-
 /-%%
 
 Main Theorem: The Prime Number Theorem in strong form.
 \begin{theorem}[PrimeNumberTheorem]\label{StrongPNT}\lean{PrimeNumberTheorem}\uses{thm:StrongZeroFree, ChebysevPsi, WeakPNT'}
 There is a constant $c > 0$ such that
 $$
-ψ (x) = x + O(x e^{-c \sqrt{\log x}})
+\psi(x) = x + O(x exp(-c sqrt(log x)))
 $$
 as $x\to \infty$.
 \end{theorem}
+
 %%-/
 /-- *** Prime Number Theorem *** The `ChebyshevPsi` function is asymptotic to `x`. -/
 theorem PrimeNumberTheorem : ∃ (c : ℝ) (hc : c > 0),
