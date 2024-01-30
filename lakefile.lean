0722de1785c2f4a111c9c30b23ff30f721be07f1
David Renshaw <dwrenshaw@gmail.com>
2024-01-30 11:36:30 -0500

    fix: remove unneeded lean_exe target to improve build time

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,7 @@ open Lake DSL
 package «PrimeNumberTheoremAnd» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «PrimeNumberTheoremAnd» where
   -- add library configuration options here
 
@@ -11,7 +12,3 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"e659b1b"
 
 meta if get_config? env = some "dev" then require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "29f7f43"
-
-@[default_target]
-lean_exe «primenumbertheoremand» where
-  root := `Main
