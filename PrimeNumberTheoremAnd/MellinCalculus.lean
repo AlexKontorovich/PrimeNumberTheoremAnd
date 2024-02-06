1318b3bca276afeb193ef755166b1fcce83c750f
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2024-02-06 17:56:16 -0500

    fixing names

--- PrimeNumberTheoremAnd/MellinCalculus.lean
+++ PrimeNumberTheoremAnd/MellinCalculus.lean
@@ -39,7 +39,7 @@ theorem MellinInversion {f : ℝ → ℂ} (σ : ℝ) (hσ : σ > 0) (hf : Contin
   sorry
 /-%%
 \begin{proof}
-\uses{PerronFormulaLtOne, PerronFormulaGtOne, MellinTransform}
+\uses{Perron.formulaLtOne, Perron.formulaGtOne, MellinTransform}
 The proof is from [Goldfeld-Kontorovich 2012].
 Integrate by parts twice.
 $$
