5de94b8da80e420392c492e0b66eb732a995108e
modderme123 <modderme123@gmail.com>
2024-02-07 14:44:25 -0500

    Revert "Revert "Merge pull request #33 from modderme123/main""
    This reverts commit 7f0b53cd3b6be53135d86c99e18140b19c28deda.

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,16 @@
 import Lake
 open Lake DSL
 
-package «PrimeNumberTheoremAnd» where
-  -- add package configuration options here
+package «PrimeNumberTheoremAnd»
 
 @[default_target]
-lean_lib «PrimeNumberTheoremAnd» where
-  -- add library configuration options here
+lean_lib «PrimeNumberTheoremAnd»
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"e659b1b"
+  "https://github.com/leanprover-community/mathlib4.git" @ "4921260"
 
-meta if get_config? env = some "dev" then require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "29f7f43"
+require EulerProducts from git
+  "https://github.com/MichaelStollBayreuth/EulerProducts.git" @ "1ff0d26"
+
+meta if get_config? env = some "dev" then require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "780bbec"
