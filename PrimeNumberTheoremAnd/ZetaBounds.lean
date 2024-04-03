13bf2afe1075b9541edd0b92db5b7352214661c9
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2024-04-03 16:30:50 -0400

    integrability_aux₀ work

--- PrimeNumberTheoremAnd/ZetaBounds.lean
+++ PrimeNumberTheoremAnd/ZetaBounds.lean
@@ -206,13 +206,18 @@ lemma Finset.sum_Ioc_add_sum_Ioc {a b c : ℤ} (f : ℤ → ℂ) (h : a ≤ b) (
 
 theorem integrability_aux₀ {a b : ℝ} (a_lt_b : a < b) :
     ∀ᵐ (x : ℝ) ∂MeasureTheory.Measure.restrict MeasureTheory.volume [[a, b]],
-      ‖(⌊x⌋ : ℂ)‖ ≤ b := by
+      ‖(⌊x⌋ : ℂ)‖ ≤ max (‖a‖ + 1) (‖b‖ + 1) := by
+  rw [MeasureTheory.ae_restrict_iff']
+  swap; · exact measurableSet_Icc
+  refine MeasureTheory.ae_of_all _ (fun x hx ↦ ?_)
+  rw [Set.uIcc_of_le a_lt_b.le, Set.mem_Icc] at hx
+
   sorry
 
 lemma integrability_aux₁ {a b : ℝ} (a_lt_b : a < b) :
     IntervalIntegrable (fun (x : ℝ) ↦ (⌊x⌋ : ℂ)) MeasureTheory.volume a b := by
   rw [intervalIntegrable_iff']
-  apply MeasureTheory.Measure.integrableOn_of_bounded (M := b)
+  apply MeasureTheory.Measure.integrableOn_of_bounded (M := max (‖a‖ + 1) (‖b‖ + 1))
   · simp only [Real.volume_interval, ne_eq, ENNReal.ofReal_ne_top, not_false_eq_true]
   · apply Measurable.aestronglyMeasurable
     apply Measurable.comp
@@ -320,7 +325,7 @@ lemma ZetaSum_aux1 {a b : ℕ} {s : ℂ} (s_ne_one : s ≠ 1) (a_lt_b : a < b) :
     · simp only [Int.floor_natCast]
   · rw [Int.floor_natCast, Int.floor_natCast]
 
-  sorry
+    sorry
 /-%%
 \begin{proof}\uses{sum_eq_int_deriv}
   Apply Lemma \ref{sum_eq_int_deriv} to the function $x \mapsto x^{-s}$.
