96355e507b1176e653fd112aac1ad38049445305
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2024-05-13 11:24:09 -0400

    ζ₀' change

--- PrimeNumberTheoremAnd/ZetaBounds.lean
+++ PrimeNumberTheoremAnd/ZetaBounds.lean
@@ -854,11 +854,12 @@ lemma hasDerivAt_Zeta0Integral {N : ℕ} (N_pos : 0 < N) {s : ℂ} (hs : s ∈ {
 noncomputable def ζ₀' (N : ℕ) (s : ℂ) : ℂ :=
     -∑ n in Finset.range (N + 1), 1 / (n : ℂ) ^ s * (n : ℝ).log +
     N ^ (1 - s) / (1 - s) ^ 2 + (N : ℝ).log * N ^ (1 - s) / (1 - s) + (N : ℝ).log * N ^ (-s) / 2
-      - s * ∫ x in Ioi (N : ℝ), x.log * (⌊x⌋ + 1 / 2 - x) / (x : ℂ) ^ (s + 1)
+      + s * ∫ x in Ioi (N : ℝ), (⌊x⌋ + 1 / 2 - x) * (x : ℂ) ^ (- s - 1) * (- Real.log x)
 
 lemma HasDerivAtZeta0 {N : ℕ} (Npos : 0 < N) {s : ℂ} (reS_pos : 0 < s.re) (s_ne_one : s ≠ 1):
     HasDerivAt (ζ₀ N) (ζ₀' N s) s := by
   unfold riemannZeta0 ζ₀'
+  apply HasDerivAt.add
 
   -- apply DifferentiableOn.sum ?_ |>.add ?_|>.add ?_|>.add ?_
   -- · intro n _
