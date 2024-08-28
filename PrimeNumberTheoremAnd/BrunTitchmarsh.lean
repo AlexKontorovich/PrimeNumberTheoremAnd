41204d9077512f29b00a2c2be31bcf83d08c546e
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-08-28 12:23:46 +0200

    Update BrunTitchmarsh.lean

--- PrimeNumberTheoremAnd/BrunTitchmarsh.lean
+++ PrimeNumberTheoremAnd/BrunTitchmarsh.lean
@@ -37,8 +37,7 @@ def primeInterSieve (x y z : ℝ) (hz : 1 ≤ z) : SelbergSieve where
       ArithmeticFunction.zeta_apply, hp.ne_zero, ↓reduceIte, Nat.cast_one,
       ArithmeticFunction.id_apply, one_div]
     apply inv_lt_one
-    norm_cast
-    exact hp.one_lt
+    exact_mod_cast hp.one_lt
   level := z
   one_le_level := hz
 
@@ -53,9 +52,9 @@ theorem siftedSum_eq_card :
     (primeInterSieve x y z hz).siftedSum =
       ((Finset.Icc (Nat.ceil x) (Nat.floor (x+y))).filter (fun d => ∀ p:ℕ, p.Prime → p ≤ z → ¬p ∣ d)).card := by
   apply Sieve.siftedSum_eq
-  exact fun _ _ => rfl
-  exact hz
-  rfl
+  · exact fun _ _ ↦ rfl
+  · exact hz
+  · rfl
 
 open Classical in
 theorem primesBetween_subset :
@@ -67,13 +66,13 @@ theorem primesBetween_subset :
   intro hx hxy hp
   by_cases hpz : p ≤ z
   · right
-    rw[Nat.le_floor_iff (by linarith)]
+    rw [Nat.le_floor_iff (by linarith)]
     have := hp.ne_zero
     exact ⟨by omega, hpz⟩
   · left
     refine ⟨⟨hx, hxy⟩, ?_⟩
     intro q hq hqz
-    rw[hp.dvd_iff_eq (hq.ne_one)]
+    rw [hp.dvd_iff_eq (hq.ne_one)]
     rintro rfl
     exact hpz hqz
 
@@ -83,16 +82,13 @@ theorem primesBetween_le_siftedSum_add :
   trans ↑((Finset.Icc (Nat.ceil x) (Nat.floor (x+y))).filter (fun d => ∀ p:ℕ, p.Prime → p ≤ z → ¬p ∣ d)
       ∪ (Finset.Icc 1 (Nat.floor z))).card
   · rw [primesBetween]
-    norm_cast
-    apply Finset.card_le_card
-    exact primesBetween_subset _ _ _ hz
+    exact_mod_cast Finset.card_le_card (primesBetween_subset _ _ _ hz)
   trans ↑((Finset.Icc (Nat.ceil x) (Nat.floor (x+y))).filter (fun d => ∀ p:ℕ, p.Prime → p ≤ z → ¬p ∣ d)).card
     + ↑(Finset.Icc 1 (Nat.floor z)).card
-  · norm_cast
-    apply Finset.card_union_le
-  rw[siftedSum_eq_card]
+  · exact_mod_cast Finset.card_union_le _ _
+  rw [siftedSum_eq_card]
   gcongr
-  rw[Nat.card_Icc]
+  rw [Nat.card_Icc]
   simp only [add_tsub_cancel_right]
   apply Nat.floor_le
   linarith
@@ -107,9 +103,8 @@ theorem Ioc_filter_dvd_eq (d a b: ℕ) (hd : d ≠ 0) :
     Nat.le_floor_iff (show 0 ≤ x+y by linarith)]
   constructor
   · intro hn
-    use  n/d
     rcases hn with ⟨⟨han, hnb⟩, hd⟩
-    refine ⟨⟨?_, ?_⟩, ?_⟩
+    refine ⟨n/d, ⟨?_, ?_⟩, ?_⟩
     · exact Nat.div_lt_div_of_lt_of_dvd hd han
     · exact Nat.div_le_div_right (Nat.le_floor hnb)
     · exact Nat.div_mul_cancel hd
@@ -129,121 +124,108 @@ theorem card_Ioc_filter_dvd (d a b: ℕ) (hd : d ≠ 0) :
 theorem multSum_eq (d : ℕ) (hd : d ≠ 0):
     (primeInterSieve x y z hz).multSum d = ↑(⌊x + y⌋₊ / d - (⌈x⌉₊ - 1) / d) := by
   unfold Sieve.multSum
-  rw[primeInterSieve]
+  rw [primeInterSieve]
   simp only [Finset.sum_boole, Nat.cast_inj]
   trans ↑(Finset.Ioc (Nat.ceil x - 1) (Nat.floor (x+y)) |>.filter (d ∣ ·) |>.card)
-  · rw [←Nat.Icc_succ_left]
+  · rw [← Nat.Icc_succ_left]
     congr
-    rw [←Nat.succ_sub]; rfl
+    rw [← Nat.succ_sub]; rfl
     simp [hx]
-  · rw[BrunTitchmarsh.card_Ioc_filter_dvd _ _ _ hd]
+  · rw [BrunTitchmarsh.card_Ioc_filter_dvd _ _ _ hd]
 
 theorem rem_eq (d : ℕ) (hd : d ≠ 0) : (primeInterSieve x y z hz).rem d = ↑(⌊x + y⌋₊ / d - (⌈x⌉₊ - 1) / d) - (↑d)⁻¹ * y := by
   unfold Sieve.rem
-  rw[multSum_eq x y z hx hz d hd]
+  rw [multSum_eq x y z hx hz d hd]
   simp [primeInterSieve, if_neg hd]
 
 theorem Nat.ceil_le_self_add_one (x : ℝ) (hx : 0 ≤ x) : Nat.ceil x ≤ x + 1 := by
   trans Nat.floor x + 1
-  · norm_cast
-    exact Nat.ceil_le_floor_add_one x
+  · exact_mod_cast Nat.ceil_le_floor_add_one x
   gcongr
-  apply Nat.floor_le hx
+  exact Nat.floor_le hx
 
 theorem floor_approx (x : ℝ) (hx : 0 ≤ x) : ∃ C, |C| ≤ 1 ∧  ↑((Nat.floor x)) = x + C := by
   use ↑(Nat.floor x) - x
   simp only [add_sub_cancel, and_true]
-  rw[abs_le]
+  rw [abs_le]
   constructor
-  · simp only [neg_le_sub_iff_le_add]
+  · rw [neg_le_sub_iff_le_add]
     linarith [Nat.lt_floor_add_one x]
-  · simp only [tsub_le_iff_right]
+  · rw [tsub_le_iff_right]
     linarith [Nat.floor_le hx]
 
 theorem ceil_approx (x : ℝ) (hx : 0 ≤ x) : ∃ C, |C| ≤ 1 ∧  ↑((Nat.ceil x)) = x + C := by
   use ↑(Nat.ceil x) - x
   simp only [add_sub_cancel, and_true]
-  rw[abs_le]
+  rw [abs_le]
   constructor
-  · simp only [neg_le_sub_iff_le_add]
+  · rw [neg_le_sub_iff_le_add]
     linarith [Nat.le_ceil x]
-  · simp only [tsub_le_iff_right]
-    rw[add_comm]
+  · rw [tsub_le_iff_right, add_comm]
     exact Nat.ceil_le_self_add_one x hx
 
 theorem nat_div_approx (a b : ℕ) : ∃ C, |C| ≤ 1 ∧ ↑(a/b) = (a/b : ℝ) + C := by
-  rw[←Nat.floor_div_eq_div (α:=ℝ)]
-  apply floor_approx (a/b:ℝ) (by positivity)
+  rw [← Nat.floor_div_eq_div (α:=ℝ)]
+  exact floor_approx (a/b:ℝ) (by positivity)
 
 theorem floor_div_approx (x : ℝ) (hx : 0 ≤ x) (d : ℕ) : ∃ C, |C| ≤ 2 ∧  ↑((Nat.floor x)/d) = x / d + C := by
   by_cases hd : d = 0
   · simp [hd]
   obtain ⟨C₁, hC₁_le, hC₁⟩ := nat_div_approx (Nat.floor x) d
   obtain ⟨C₂, hC₂_le, hC₂⟩ := floor_approx x hx
-  rw[hC₁, hC₂]
-  use  C₁ + C₂/d
-  refine ⟨?_, by ring⟩
+  rw [hC₁, hC₂]
+  refine ⟨C₁ + C₂/d, ?_, by ring⟩
   have : |C₁ + C₂/d| ≤ |C₁| + |C₂/d| := abs_add C₁ (C₂ / ↑d)
   have : |C₂/d| ≤ |C₂| := by
-    rw[abs_div]
-    apply div_le_self
-    · exact abs_nonneg C₂
-    · simp only [Nat.abs_cast, Nat.one_le_cast]
-      omega
+    rw [abs_div]
+    refine div_le_self (abs_nonneg C₂) ?_
+    simp only [Nat.abs_cast, Nat.one_le_cast]
+    omega
   linarith
 
 theorem abs_rem_le {d : ℕ} (hd : d ≠ 0) : |(primeInterSieve x y z hz).rem d| ≤ 5 := by
-  rw[rem_eq _ _ _ hx hz _ hd]
-
+  rw [rem_eq _ _ _ hx hz _ hd]
   have hpush : ↑(⌊x + y⌋₊ / d - (⌈x⌉₊ - 1) / d) = ( ↑(⌊x + y⌋₊ / d) - ↑((⌈x⌉₊ - 1) / d) : ℝ) := by
     rw [Nat.cast_sub]
     gcongr
-    rw[Nat.le_floor_iff]
-    rw[←add_le_add_iff_right 1]
-    norm_cast
-    rw [Nat.sub_add_cancel (by simp [hx])]
+    rw [Nat.le_floor_iff, ← add_le_add_iff_right 1]
+    rw_mod_cast [Nat.sub_add_cancel (by simp [hx])]
     linarith [Nat.ceil_le_self_add_one x (le_of_lt hx)]
     linarith
-
-  rw[hpush]
+  rw [hpush]
   obtain ⟨C₁, hC₁_le, hC₁⟩ := floor_div_approx (x + y) (by linarith) d
   obtain ⟨C₂, hC₂_le, hC₂⟩ := nat_div_approx (Nat.ceil x - 1) d
   obtain ⟨C₃, hC₃_le, hC₃⟩ := ceil_approx (x) (by linarith)
-  rw[hC₁, hC₂, Nat.cast_sub, hC₃]
-  ring_nf
-  have : |(↑d)⁻¹ - (↑d)⁻¹ * C₃ + (C₁ - C₂)| ≤ |(↑d)⁻¹ - (↑d)⁻¹*C₃| + |C₁ - C₂| := by
-    apply (abs_add _ _)
-  have : |(↑d)⁻¹ - (↑d)⁻¹*C₃| ≤ |(↑d)⁻¹| + |(↑d)⁻¹*C₃| := abs_sub _ _
-  have : |C₁ - C₂| ≤ |C₁| + |C₂| := abs_sub _ _
-  have : |(d:ℝ)⁻¹| ≤ 1 := by
-    rw[abs_inv]
-    simp only [Nat.abs_cast]
-    apply Nat.cast_inv_le_one
-  have : |(↑d)⁻¹*C₃| ≤ |C₃| := by
-    rw[inv_mul_eq_div, abs_div]
-    apply div_le_self
-    · exact abs_nonneg _
-    · simp only [Nat.abs_cast, Nat.one_le_cast]
+  rw [hC₁, hC₂, Nat.cast_sub, hC₃]
+  · ring_nf
+    have : |(↑d)⁻¹ - (↑d)⁻¹ * C₃ + (C₁ - C₂)| ≤ |(↑d)⁻¹ - (↑d)⁻¹*C₃| + |C₁ - C₂| := abs_add _ _
+    have : |(↑d)⁻¹ - (↑d)⁻¹*C₃| ≤ |(↑d)⁻¹| + |(↑d)⁻¹*C₃| := abs_sub _ _
+    have : |C₁ - C₂| ≤ |C₁| + |C₂| := abs_sub _ _
+    have : |(d:ℝ)⁻¹| ≤ 1 := by
+      rw [abs_inv, Nat.abs_cast]
+      exact Nat.cast_inv_le_one _
+    have : |(↑d)⁻¹*C₃| ≤ |C₃| := by
+      rw [inv_mul_eq_div, abs_div]
+      refine div_le_self (abs_nonneg _) ?_
+      rw [Nat.abs_cast, Nat.one_le_cast]
       omega
-  linarith
+    linarith
   · simp [hx]
 
 end Remainder
 
 theorem boudingSum_ge : (primeInterSieve x y z hz).selbergBoundingSum ≥ Real.log z / 2 := by
   apply boundingSum_ge_log
-  · rfl
+  · exact rfl
   · intro p hpp hp
     erw [prime_dvd_primorial_iff]
-    apply Nat.le_floor
-    exact hp
-    exact hpp
+    · exact Nat.le_floor hp
+    · exact hpp
 
 theorem primeSieve_rem_sum_le :
     ∑ d in (primeInterSieve x y z hz).prodPrimes.divisors, (if (d : ℝ) ≤ z then (3:ℝ) ^ ω d * |(primeInterSieve x y z hz).rem d| else 0)
       ≤ 5 * z * (1+Real.log z)^3 := by
-  apply rem_sum_le_of_const (primeInterSieve x y z hz) 5 ?_
-  intro d hd
+  refine rem_sum_le_of_const (primeInterSieve x y z hz) 5 (fun d hd ↦ ?_)
   apply abs_rem_le <;> linarith
 
 theorem siftedSum_le (hz : 1 < z) :
@@ -254,16 +236,16 @@ theorem siftedSum_le (hz : 1 < z) :
   gcongr
   · linarith [Real.log_pos hz]
   · rfl
-  · apply boudingSum_ge
-  · apply primeSieve_rem_sum_le x y z hx hy
+  · exact boudingSum_ge _ _ _ _
+  · exact primeSieve_rem_sum_le x y z hx hy _
 
 theorem primesBetween_le (hz : 1 < z) :
     primesBetween x (x+y) ≤ 2 * y / Real.log z + 6 * z * (1+Real.log z)^3 := by
   have : z ≤ z * (1+Real.log z)^3 := by
     apply le_mul_of_one_le_right
     · linarith
-    apply one_le_pow_of_one_le _ _
-    linarith [Real.log_nonneg (by linarith)]
+    · apply one_le_pow_of_one_le _ _
+      linarith [Real.log_nonneg (by linarith)]
   linarith [siftedSum_le _ _ _ hx hy hz, primesBetween_le_siftedSum_add x y z hz.le]
 
 theorem primesBetween_one (n : ℕ) : primesBetween 1 n = ((Finset.range (n+1)).filter Nat.Prime).card := by
@@ -272,18 +254,14 @@ theorem primesBetween_one (n : ℕ) : primesBetween 1 n = ((Finset.range (n+1)).
   ext p
   simp only [Nat.ceil_one, Nat.floor_coe, Finset.mem_filter, Finset.mem_Icc, Finset.mem_range,
     and_congr_left_iff]
-  intro hp
-  refine ⟨?_, ?_⟩
-  · exact fun h => by omega
-  · refine fun h => ⟨by have := hp.pos; omega, by omega⟩
+  exact fun hp ↦ ⟨fun h => by omega, fun h => ⟨by have := hp.pos; omega, by omega⟩⟩
 
 theorem primesBetween_mono_right (a b c : ℝ) (hbc : b ≤ c) : primesBetween a b ≤ primesBetween a c := by
   dsimp only [primesBetween]
   apply Finset.card_le_card
   intro p
   simp only [Finset.mem_filter, Finset.mem_Icc, Nat.ceil_le, and_imp]
-  intro ha hb hp
-  refine ⟨⟨ha, hb.trans (Nat.floor_mono hbc)⟩, hp⟩
+  exact fun ha hb hp ↦ ⟨⟨ha, hb.trans (Nat.floor_mono hbc)⟩, hp⟩
 
 theorem tmp (N : ℕ) : ((Finset.range N).filter Nat.Prime).card ≤ 4 * (N / Real.log N) + 6 *(N ^ (1/2 : ℝ) * (1 + 1/2 * Real.log N)^3) := by
   trans ↑((Finset.range (N+1)).filter Nat.Prime).card
