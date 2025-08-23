6bd038fd21e76dfb0d6052d78cd6196bae99c3b6
Alastair Irving <29164966+ajirving@users.noreply.github.com>
2025-08-23 12:49:31 +0100

    Use LogDerivZetaBnd' rather than Bnd

--- PrimeNumberTheoremAnd/ZetaBoundsUnused.lean
+++ PrimeNumberTheoremAnd/ZetaBoundsUnused.lean
@@ -22,7 +22,7 @@ lemma LogDerivZetaBndAlt :
     (fun (t : ℝ) ↦ ζ' (σ + t * I) / ζ (σ + t * I)) =O[cocompact ℝ ⊓
       Filter.principal {t | 1 - A / Real.log |t| ^ 9 < σ}]
         fun t ↦ Real.log |t| ^ 9 := by
-  obtain ⟨A, hA, C, _, h⟩ := LogDerivZetaBnd
+  obtain ⟨A, hA, C, _, h⟩ := LogDerivZetaBnd'
   refine ⟨A, hA.1, fun σ ⟨σ_ge, σ_lt⟩ ↦ ?_⟩
   -- This could all be done much cleaner; TODO: refactor
   rw [Asymptotics.isBigO_iff]
@@ -40,7 +40,7 @@ lemma LogDerivZetaBndAlt :
       refine ⟨4, fun b hb ↦ ?_⟩
       rwa [_root_.abs_of_nonneg (by linarith)]
   simp only [mem_setOf_eq] at ht
-  convert h σ t (by linarith [mem_Ici.mp t_ge]) ⟨ht.le, σ_lt⟩
+  convert h σ t (by linarith [mem_Ici.mp t_ge]) ⟨ht.le, (by bound)⟩
   simp only [mem_setOf_eq] at t_ge
   have := Real.log_nonneg (by linarith : 1 ≤ |t|)
   simp only [Real.norm_eq_abs, norm_pow, abs_eq_self.mpr, this]
