19381d4583001adcedd7a92c718131304513a42a
Maksym Radziwill <maksym.radziwill@gmail.com>
2025-10-09 21:29:34 -0500

    Same modifications in the proposed DerivativeBound.lean file

--- PrimeNumberTheoremAnd/StrongPNT/DerivativeBound.lean
+++ PrimeNumberTheoremAnd/StrongPNT/DerivativeBound.lean
@@ -18,11 +18,12 @@ import Mathlib.Analysis.Analytic.Within
 import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.Analysis.Complex.AbsMax
 
-theorem derivativeBound (R M r r' : ℝ) (pos_r : 0 < r) (r_le_r' : r < r')
-  (r'_le_R : r' < R) (metric_ball : Metric.closedBall 0 R) (f : ℂ → ℂ) 
+theorem derivativeBound {R M r r' : ℝ} {z : ℂ} {f : ℂ → ℂ}
   (analytic_f : AnalyticOn ℂ f (Metric.closedBall 0 R)) (f_zero_at_zero : f 0 = 0) 
-  (re_f_le_M : ∀z ∈ Metric.closedBall 0 R, (f z).re ≤ M):
-  ∀z ∈ Metric.closedBall 0 r, ‖(deriv f) z‖ ≤ 2 * M * (r')^2 / ((R - r') * (r' - r)^2) := by sorry
+  (re_f_le_M : ∀z ∈ Metric.closedBall 0 R, (f z).re ≤ M) 
+  (pos_r : 0 < r) (z_in_r : z ∈ Metric.closedBall 0 r)
+  (r_le_r' : r < r') (r'_le_R : r' < R) : 
+  ‖(deriv f) z‖ ≤ 2 * M * (r')^2 / ((R - r') * (r' - r)^2) := by sorry
 
 
   
