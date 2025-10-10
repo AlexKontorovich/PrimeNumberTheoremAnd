91e8ba6fb292a380fecac2c4104a1873eb4f418f
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-10-10 22:05:32 +0200

    chore: improve formatting

--- PrimeNumberTheoremAnd/Mathlib/Analysis/SpecialFunctions/Log/Basic.lean
+++ PrimeNumberTheoremAnd/Mathlib/Analysis/SpecialFunctions/Log/Basic.lean
@@ -8,7 +8,7 @@ open Filter Real
 
 /-- log^b x / x^a goes to zero at infinity if a is positive. -/
 theorem Real.tendsto_pow_log_div_pow_atTop (a : ℝ) (b : ℝ) (ha : 0 < a) :
-Filter.Tendsto (fun x ↦ log x ^ b / x^a) Filter.atTop (nhds 0) := by
+    Filter.Tendsto (fun x ↦ log x ^ b / x^a) Filter.atTop (nhds 0) := by
   convert squeeze_zero' (f := fun x ↦ log x ^ b / x^a) (g := fun x ↦ (log x ^ ⌈ b/a ⌉₊ / x)^a ) (t₀ := atTop) ?_ ?_ ?_
   · simp
     use 1
