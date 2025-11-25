d1773e3fb31cc3e53ae541f35d493be2f5e4962c
Terence Tao <tao@math.ucla.edu>
2025-11-24 19:49:50 -0800

    add stubs

--- PrimeNumberTheoremAnd/PrimaryDefinitions.lean
+++ PrimeNumberTheoremAnd/PrimaryDefinitions.lean
@@ -1,4 +1,7 @@
-import Mathlib.Tactic
+import Mathlib.NumberTheory.ArithmeticFunction
+import Mathlib.NumberTheory.LSeries.PrimesInAP
+
+open ArithmeticFunction hiding log
 
 /-%%
 \section{Definitions}
@@ -7,3 +10,7 @@ import Mathlib.Tactic
 /-%%
 In this section we define the basic types of primary estimates we will work with in the project.
 %%-/
+
+noncomputable def ψ (x : ℝ) : ℝ := ∑ᶠ (n : ℕ) (_: n < x), Λ n
+
+noncomputable def PNT_error (x : ℝ) : ℝ := |ψ x - x|
