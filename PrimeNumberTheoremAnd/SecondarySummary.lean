8f9e5f0628adf6e46b3c9e5690585275039046bb
Terence Tao <tao@math.ucla.edu>
2026-01-01 15:00:20 -0800

    Add FKS results; standardize naming in secondary summary

--- PrimeNumberTheoremAnd/SecondarySummary.lean
+++ PrimeNumberTheoremAnd/SecondarySummary.lean
@@ -24,7 +24,7 @@ JY: D. R. Johnston, A. Yang, Some explicit estimates for the error term in the p
 open Real
 
 /-%%
-\begin{theorem}[PT Corollary 2]\label{thm:pt_2}\lean{PT_Cor2}\leanok
+\begin{theorem}[PT Corollary 2]\label{thm:pt_2}\lean{PT.corollary_1_2}\leanok
 One has
 \[
 |\pi(x) - \mathrm{Li}(x)| \leq 235 x (\log x)^{0.52} \exp(-0.8 \sqrt{\log x})
@@ -33,10 +33,10 @@ for all $x \geq \exp(2000)$.
 \end{theorem}
 %%-/
 
-theorem PT_Cor2: Eπ.classicalBound 235 1.52 0.8 1 (exp 2000) := sorry
+theorem PT.corollary_1_2: Eπ.classicalBound 235 1.52 0.8 1 (exp 2000) := sorry
 
 /-%%
-\begin{theorem}[JY Corollary 1.3]\label{thm:jy_13}\lean{JY_Cor13}\leanok
+\begin{theorem}[JY Corollary 1.3]\label{thm:jy_13}\lean{JY.corollary_1_3}\leanok
 One has
 \[
 |\pi(x) - \mathrm{Li}(x)| \leq 9.59 x (\log x)^{0.515} \exp(-0.8274 \sqrt{\log x})
@@ -45,10 +45,10 @@ for all $x \geq 2$.
 \end{theorem}
 %%-/
 
-theorem JY_Cor13: Eπ.classicalBound 9.59 1.515 0.8274 1 2 := sorry
+theorem JY.corollary_1_3: Eπ.classicalBound 9.59 1.515 0.8274 1 2 := sorry
 
 /-%%
-\begin{theorem}[JY Theorem 1.4]\label{thm:jy_14}\lean{JY_Thm14}\leanok
+\begin{theorem}[JY Theorem 1.4]\label{thm:jy_14}\lean{JY.theorem_1_4}\leanok
 One has
 \[
 |\pi(x) - \mathrm{Li}(x)| \leq 0.028 x (\log x)^{0.801} \exp(-0.1853 \log^{3/5} x / (\log \log x)^{1/5}))
@@ -57,12 +57,12 @@ for all $x \geq 2$.
 \end{theorem}
 %%-/
 
-theorem JY_Thm14: Eπ.vinogradovBound 0.028 0.801 0.1853 2 := sorry
+theorem JY.theorem_1_4: Eπ.vinogradovBound 0.028 0.801 0.1853 2 := sorry
 
 /-%% TODO: input other results from JY %%-/
 
 /-%%
-\begin{theorem}[FKS2 Corollary 22]\label{thm:fks2_22}\lean{FKS2_Cor22}\leanok
+\begin{theorem}[FKS2 Corollary 22]\label{thm:fks2_22}\lean{FKS2.corollary_22}\leanok
 One has
 \[
 |\pi(x) - \mathrm{Li}(x)| \leq 9.2211 x \sqrt{\log x} \exp(-0.8476 \sqrt{\log x})
@@ -71,10 +71,10 @@ for all $x \geq 2$.
 \end{theorem}
 %%-/
 
-theorem FKS2_Cor22: Eπ.classicalBound 9.2211 1.5 0.8476 1 2 := sorry
+theorem FKS2.corollary_22: Eπ.classicalBound 9.2211 1.5 0.8476 1 2 := sorry
 
 /-%%
-\begin{theorem}[FKS2 Corollary 26]\label{thm:fks2_26}\lean{FKS2_Cor26}\leanok
+\begin{theorem}[FKS2 Corollary 26]\label{thm:fks2_26}\lean{FKS2.corollary_26}\leanok
 One has
 \[
 |\pi(x) - \mathrm{Li}(x)| \leq 0.4298 \frac{x}{\log x}
@@ -83,12 +83,12 @@ for all $x \geq 2$.
 \end{theorem}
 %%-/
 
-theorem FKS2_Cor226 : Eπ.bound 0.4298 2 := sorry
+theorem FKS2.corollary_26 : Eπ.bound 0.4298 2 := sorry
 
 
 
 /-%%
-\begin{theorem}[Dusart Proposition 5.4]\label{thm:Dusart}
+\begin{theorem}[Dusart Proposition 5.4]\label{thm:Dusart}\lean{Dusart.proposition_5_4}\leanok
 There exists a constant \(X_0\) (one may take \(X_0 = 89693\)) with the following property:
 for every real \(x \ge X_0\), there exists a prime \(p\) with
 \[
@@ -97,6 +97,6 @@ for every real \(x \ge X_0\), there exists a prime \(p\) with
 \end{theorem}
 %%-/
 
-theorem Dusart_thm : HasPrimeInInterval.log_thm 89693 3 := sorry
+theorem Dusart.proposition_5_4 : HasPrimeInInterval.log_thm 89693 3 := sorry
 
 /-%% TODO: input other results from Dusart %%-/
