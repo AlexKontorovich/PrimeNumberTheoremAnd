d41617103a2fe61288a425858f5ebea85f58b4f8
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2026-01-01 19:22:53 +0100

    Improve formatting of docstrings and function definitions
    Reformatted multiline docstrings for better readability and split long function definitions across multiple lines. These changes enhance code clarity and maintainability without altering functionality.

--- PrimeNumberTheoremAnd/FKS2.lean
+++ PrimeNumberTheoremAnd/FKS2.lean
@@ -21,7 +21,9 @@ theorem fks2_rem : ∀ x > 2, li x - Li x = li 2 := sorry
 @[blueprint
   "fks2-eq-19"
   (title := "Dawson function, FKS2 (19)")
-  (statement := /-- The Dawson function $D_+ : \mathbb{R} \to \mathbb{R}$ is defined by the formula $D_+(x) := e^{-x^2} \int_0^x e^{t^2}\ dt$. -/)]
+  (statement := /--
+  The Dawson function $D_+ : \mathbb{R} \to \mathbb{R}$ is defined by the formula
+  $D_+(x) := e^{-x^2} \int_0^x e^{t^2}\ dt$. -/)]
 noncomputable def dawson (x : ℝ) : ℝ := exp (-x ^ 2) * ∫ t in 0..x, exp (t ^ 2)
 
 
@@ -31,20 +33,26 @@ noncomputable def dawson (x : ℝ) : ℝ := exp (-x ^ 2) * ∫ t in 0..x, exp (t
   (statement := /--
   For $x_0,x_1 > 0$, we define
   $$ mu_{asymp}(x_0,x_1) :=
-  \frac{x_0 \log(x_1)}{\epsilon_{\theta,asymp}(x_1) x_1 \log(x_0)} \left|\frac{\pi(x_0) - \Li(x_0)}{x_0/\log x_0} - \frac{\theta(x_0) - x_0}{x_0}\right| + \frac{2D_+(\sqrt{\log(x_1)} - \frac{C}{2\sqrt{R}}}{\sqrt{\log x_1}}$$.
+  \frac{x_0 \log(x_1)}{\epsilon_{\theta,asymp}(x_1) x_1 \log(x_0)}
+  \left|\frac{\pi(x_0) - \Li(x_0)}{x_0/\log x_0} - \frac{\theta(x_0) - x_0}{x_0}\right| +
+  \frac{2D_+(\sqrt{\log(x_1)} - \frac{C}{2\sqrt{R}}}{\sqrt{\log x_1}}$$.
   -/)]
-noncomputable def mu_asymp (A B C R x₀ x₁ : ℝ) : ℝ := (x₀ * log x₁) / ((admissible_bound A B C R x₁) * x₁ * log x₀) * |Eπ x₀ - Eθ x₀| + 2 * (dawson (sqrt (log x₁) - C / (2 * sqrt R))) / (sqrt (log x₁))
+noncomputable def mu_asymp (A B C R x₀ x₁ : ℝ) : ℝ :=
+  (x₀ * log x₁) / ((admissible_bound A B C R x₁) * x₁ * log x₀) * |Eπ x₀ - Eθ x₀| +
+    2 * (dawson (sqrt (log x₁) - C / (2 * sqrt R))) / (sqrt (log x₁))
 
 
 @[blueprint
   "fks2-theorem-3"
   (title := "FKS2 Theorem 3")
   (statement := /--
-  If $B \geq \max(3/2, 1 + C^2/16 R)$, $x_0 > 0$, and one has an admissible asymptotic bound with parameters $A,B,C,x_0$ for $E_\theta$, and
+  If $B \geq \max(3/2, 1 + C^2/16 R)$, $x_0 > 0$, and one has an admissible asymptotic bound with
+  parameters $A,B,C,x_0$ for $E_\theta$, and
   $$ x_1 \geq \max( x_0, \exp( (1 + \frac{C}{2\sqrt{R}}))^2),$$
   then
   $$ E_\pi(x) \leq \epsilon_{\theta,asymp}(x_1) ( 1 + \mu_{asymp}(x_0,x_1) ) $$
-  for all $x \geq x_1$.  In other words, we have an admissible bound with parameters $(1+\mu_{asymp}(x_0,x_1))A, B, C, x_1$ for $E_\pi$.
+  for all $x \geq x_1$.  In other words, we have an admissible bound with parameters
+  $(1+\mu_{asymp}(x_0,x_1))A, B, C, x_1$ for $E_\pi$.
   -/)]
 theorem fks_theorem_3 (A B C R x₀ x₁ : ℝ)
   (hB : B ≥ max (3 / 2) (1 + C ^ 2 / (16 * R)))
