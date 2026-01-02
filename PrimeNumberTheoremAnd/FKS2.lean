a1830056a079c40c4e40f23d23c5b7725bd8e990
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2026-01-02 01:23:57 +0100

    Update FKS2.lean

--- PrimeNumberTheoremAnd/FKS2.lean
+++ PrimeNumberTheoremAnd/FKS2.lean
@@ -7,7 +7,7 @@ In this file we record the implications in the paper
 
 FKS2: Fiori--Kadiri--Swidninsky arXiv:2206.12557
 
-that allow one to convert primary bounds on Eψ into secondary bounds on Eπ, Eθ.
+that allow one to convert primary bounds on $E_\psi$ into secondary bounds on $E_\pi$, $E_\theta$.
 -/
 
 open Real
