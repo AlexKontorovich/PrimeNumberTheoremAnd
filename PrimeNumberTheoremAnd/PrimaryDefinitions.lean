08a67ddeb1293b516241f464fa8ed97f9b1bd691
Terence Tao <tao@math.ucla.edu>
2026-01-03 14:43:28 -0800

    some more defs

--- PrimeNumberTheoremAnd/PrimaryDefinitions.lean
+++ PrimeNumberTheoremAnd/PrimaryDefinitions.lean
@@ -2,6 +2,7 @@ import Architect
 import Mathlib.NumberTheory.ArithmeticFunction.VonMangoldt
 import Mathlib.NumberTheory.Chebyshev
 import Mathlib.NumberTheory.LSeries.RiemannZeta
+import Mathlib.Analysis.Meromorphic.Order
 
 open Real
 open ArithmeticFunction hiding log
@@ -41,8 +42,32 @@ noncomputable def admissible_bound (A B C R : ℝ) (x : ℝ) := A * (log x / R) 
   -/)]
 def Eψ.classicalBound (A B C R x₀ : ℝ) : Prop := ∀ x ≥ x₀, Eψ x ≤ admissible_bound A B C R x
 
+/-- May need to guard against junk value at s=1 -/
+noncomputable def riemannZeta.zeroes : Set ℂ := {s : ℂ | riemannZeta s = 0 }
+
+def WithTop.top_to_zero {α : Type*} [Zero α] (a : WithTop α) : α := match a with
+| ⊤ => 0
+| (some x) => x
+
+noncomputable def riemannZeta.order (s : ℂ) : ℤ := (meromorphicOrderAt (riemannZeta) s).top_to_zero
+
 @[blueprint
   "classical-zero-free-region"
   (title := "Section 1.1, FKS2")
   (statement := /-- We say that one has a classical zero-free region with parameter $R$ if $zeta(s)$ has no zeroes in the region $Re(s) \geq 1 - 1 / R * \log |\Im s|$ for $\Im(s) > 3$. -/)]
 noncomputable def riemannZeta.classicalZeroFree (R : ℝ) := ∀ (σ t : ℝ), t ≥ 3 → σ ≥ 1 / (R * log t) → riemannZeta (σ + t * Complex.I) ≠ 0
+
+@[blueprint
+  "zero-counting-function"
+  (title := "Zero counting function N")
+  (statement := /-- The number of zeroes of imaginary part between 0 and T, counting multiplicity -/)]
+noncomputable def riemannZeta.N (T : ℝ) : ℝ := ∑' ρ : { s:ℂ | s ∈ riemannZeta.zeroes ∧ 0 < s.im ∧ s.im < T }, riemannZeta.order ρ
+
+noncomputable def riemannZeta.R (b₁ b₂ b₃ T : ℝ) : ℝ := b₁ * log T + b₂ * log (log T) + b₃
+
+@[blueprint
+  "Riemann-von-Mangoldt-estimate"
+  (title := "Riemann von Mangoldt estimate")
+  (statement := /-- An estimate of the form $N(T) - \frac{T}{2\pi} \log \frac{T}{2\pi e} + \frac{7}{8}| ≤ b_1 \log T + b_2 \log\log T + b_3$ for $T \geq 2$. -/)]
+def riemannZeta.Riemann_vonMangoldt_bound (b₁ b₂ b₃ : ℝ) : Prop :=
+  ∀ T ≥ 2, |riemannZeta.N T - (T / (2 * π) * log (T / (2 * π)) - T / (2 * π) + 7/8)| ≤ R b₁ b₂ b₃ T
