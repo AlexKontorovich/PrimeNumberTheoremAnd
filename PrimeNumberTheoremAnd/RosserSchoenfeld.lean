8d7aabc1b63d4bb1f416794e31ab94c9044845b5
Terence Tao <tao@math.ucla.edu>
2026-01-03 14:43:59 -0800

    add an RS estimate

--- PrimeNumberTheoremAnd/RosserSchoenfeld.lean
+++ PrimeNumberTheoremAnd/RosserSchoenfeld.lean
@@ -1,5 +1,5 @@
 import Architect
-import Mathlib.Tactic
+import PrimeNumberTheoremAnd.PrimaryDefinitions
 
 blueprint_comment /--
 \section{The arguments of Rosser and Schoenfeld}
@@ -9,3 +9,11 @@ blueprint_comment /--
 In this section we formalize the arguments of Rosser and Schoenfeld that can convert primary
 estimates to secondary estimates, with an emphasis on parameter flexibility.
 -/
+
+namespace RS
+
+@[blueprint
+  "RS_theorem_19"
+  (title := "Rosser--Schoenfeld Theorem 19")
+  (statement := /-- One has a Riemann von Mangoldt estimate with parameters 0.137, 0.443, and 1.588. --/)]
+theorem theorem_19 : riemannZeta.Riemann_vonMangoldt_bound 0.137 0.443 1.588
