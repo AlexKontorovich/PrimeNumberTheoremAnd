87b4339bba8ba7db6332c29d889bbeb6720731d4
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2026-01-08 09:27:56 -0500

    merging

--- PrimeNumberTheoremAnd/HadamardFactorization.lean
+++
@@ -1,9 +0,0 @@
-import Architect
-import Mathlib.Init
-
-blueprint_comment /--
-
-In this file, we prove the Hadamard Factorization theorem for functions of finite order,
-and prove that the zeta function is such.
-
--/
