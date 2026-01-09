c0b9792684f0e83d1d1317b78b906af545a99971
Terence Tao <tao@math.ucla.edu>
2026-01-08 16:17:02 -0800

    commenting out some text to debug

--- PrimeNumberTheoremAnd/ZetaAppendix.lean
+++ PrimeNumberTheoremAnd/ZetaAppendix.lean
@@ -71,34 +71,37 @@ theorem lemma_1 {s : ℂ} (hs : 0 ≤ s.re) {a b : ℝ} (hab : a < b)
 @[blueprint
   "prop:dadaro"
   (title := "Appendix Proposition 2")
-  (statement := /--  Let $s = \sigma + i \tau$, $\sigma\geq 0$, $\tau\in \mathbb{R}$.
- Let $a\in \mathbb{Z} + \frac{1}{2}$ with $a>\frac{|\tau|}{2\pi}$. Then
-\begin{equation}\label{eq:londie}\zeta(s) = \sum_{n\leq a} \frac{1}{n^s} - \frac{a^{1-s}}{1-s} + c_\rho a^{-s} + O^*\left(\frac{C_{\sigma,\rho}}{a^{\sigma+1}}\right),\end{equation}
-where $\rho = \frac{\tau}{2\pi a}$,
-$c_\rho = \frac{i}{2}  \left(\frac{1}{\sin \pi \rho} - \frac{1}{\pi \rho}\right)$
-and $C_{\sigma,\rho}= \frac{\sigma}{2} \left(\frac{1}{\sin^2\pi \rho} - \frac{1}{(\pi \rho)^2}\right) + \frac{|\rho|}{2\pi^2} \left(\frac{1}{(1-|\rho|)^3} + 2\zeta(3)-1\right)$.
-.-/)
-  (proof := /-- We can assume $\sigma>0$, in that we can then address the case $\sigma=0$ by letting $\sigma\to 0^+$.
+  (statement := /-- Temp -/)
+--   (statement := /--
+-- Let $s = \sigma + i \tau$, $\sigma\geq 0$, $\tau\in \mathbb{R}$.
+--  Let $a\in \mathbb{Z} + \frac{1}{2}$ with $a>\frac{|\tau|}{2\pi}$. Then
+-- \begin{equation}\label{eq:londie}\zeta(s) = \sum_{n\leq a} \frac{1}{n^s} - \frac{a^{1-s}}{1-s} + c_\rho a^{-s} + O^*\left(\frac{C_{\sigma,\rho}}{a^{\sigma+1}}\right),\end{equation}
+-- where $\rho = \frac{\tau}{2\pi a}$,
+-- $c_\rho = \frac{i}{2}  \left(\frac{1}{\sin \pi \rho} - \frac{1}{\pi \rho}\right)$
+-- and $C_{\sigma,\rho}= \frac{\sigma}{2} \left(\frac{1}{\sin^2\pi \rho} - \frac{1}{(\pi \rho)^2}\right) + \frac{|\rho|}{2\pi^2} \left(\frac{1}{(1-|\rho|)^3} + 2\zeta(3)-1\right)$.
+-- .-/)
+--   (proof := /-- We can assume $\sigma>0$, in that we can then address the case $\sigma=0$ by letting $\sigma\to 0^+$.
 
-Let $b>a$, $b\in \mathbb{Z} + \frac{1}{2}$. Clearly $\sum_{n\leq b} 1/n^s = \zeta(s) - \sum_{n>b} 1/n^s$ for $\Re s > 1$, and so, by Euler-Maclaurin and $\int_b^\infty \frac{dy}{y^s} = -\frac{b^{1-s}}{1-s}$,
-$$\sum_{n\leq b} \frac{1}{n^s} = \zeta(s) + \frac{b^{1-s}}{1-s} + s \int_b^\infty \left(\{y\}-\frac{1}{2}\right) \frac{dy}{y^{s+1}};$$
-by analytic continuation, this equation actually holds for $\Re s>0$. Hence, for our $s$,
-$$\sum_{n\leq a} \frac{1}{n^s} = -\sum_{a < n\leq b} \frac{1}{n^s} + \zeta(s) + \frac{b^{1-s}}{1-s} + O\left(\frac{|s|}{\sigma b^\sigma}\right).$$
-Let $f(y) = 1_{[a,b]}(y)/y^s$. Then, by Poisson summation,
-$$\sum_{a < n\leq b} \frac{1}{n^s} = \sum_{n\in \mathbb{Z}} f(n) = \lim_{N\to \infty} \sum_{n=-N}^N \widehat{f}(n).$$
-(See, e.g., Thm.~D.3 of MR2378655; notice $f$ is continuous at every integer.) Clearly
-$$\widehat{f}(0) = \int_{\mathbb{R}} f(y) dy = \int_a^b \frac{dy}{y^s} = \frac{b^{1-s}-a^{1-s}}{1-s},$$
-and so
-$$\sum_{n\leq a} \frac{1}{n^s} = \zeta(s) + \frac{a^{1-s}}{1-s} - \lim_{N\to \infty} \sum_{n=1}^N (\widehat{f}(n) + \widehat{f}(-n)) + O\left(\frac{|s|}{\sigma b^\sigma}\right).$$
-Applying Lemma \ref{lem:applem} and multiplying by $2$ (since $e(-n t)+e(n t) = 2 \cos 2\pi n t$), we see that
-$$\begin{aligned}\widehat{f}(n) + \widehat{f}(-n) &=
- \frac{(-1)^{n+1} (a^{-s} +O^*\left(\frac{a}{b^{\sigma+1}}\right))}{2\pi i} \frac{2\rho}{n^2 - \rho^2} \\
- &+ \frac{a^{-\sigma-1}}{2\pi^2} O^*\left(\frac{\sigma}{(n-\rho)^2} + \frac{\sigma}{(n+\rho)^2} + \frac{|\rho|}{(n-\rho)^3} + \frac{|\rho|}{(n+\rho)^3}\right).\end{aligned}$$
-(The term involving $b$ really gets divided by $n^2-\left(\frac{\tau}{2\pi b}\right)^2$, but that is larger than $n^2 - \rho^2$.) We have Euler's identities $\sum_{n=1}^\infty \frac{(-1)^{n+1} \cdot 2\rho}{n^2-\rho^2} = \frac{\pi}{\sin \pi \rho} - \frac{1}{\rho}$ and $\sum_{n=-\infty}^\infty \frac{1}{(n-\rho)^2} = \frac{\pi^2}{\sin^2 \pi \rho}$. Moreover, $$\begin{aligned}\sum_{n=1}^\infty \left(\frac{1}{(n-\rho)^3} + \frac{1}{(n+\rho)^3}\right) &=\frac{1}{(1-|\rho|)^3} + \sum_{n=1}^\infty \left(\frac{1}{(n+1-|\rho|)^3} + \frac{1}{(n+|\rho|)^3}\right)\\ &\leq \frac{1}{(1-|\rho|)^3} + 2\zeta(3)-1,\end{aligned}$$
-since $\frac{1}{(n+1-t)^3} + \frac{1}{(n+t)^3}$ reaches its maximum on $[0,1]$ at $t=0,1$. Thus, since $|\rho|<1$,
-$$\begin{aligned}\lim_{N\to \infty} \sum_{n=1}^N (\widehat{f}(n) + \widehat{f}(-n)) &= \frac{a^{-s}+O(b^{-\sigma-1})}{2 i} \left(\frac{1}{\sin \pi \rho} - \frac{1}{\pi \rho}\right) + O^*\left(\frac{C_{\sigma,\rho}}{a^{\sigma+1}}\right),\end{aligned}$$
-where $C_{\sigma,\rho}= \frac{\sigma}{2} \left(\frac{1}{\sin^2\pi \rho} - \frac{1}{(\pi \rho)^2}\right) + \frac{|\rho|}{2\pi^2} \left(\frac{1}{(1-|\rho|)^3} + 2\zeta(3)-1\right)$. We let $b\to \infty$ and are done.
-  -/)]
+-- Let $b>a$, $b\in \mathbb{Z} + \frac{1}{2}$. Clearly $\sum_{n\leq b} 1/n^s = \zeta(s) - \sum_{n>b} 1/n^s$ for $\Re s > 1$, and so, by Euler-Maclaurin and $\int_b^\infty \frac{dy}{y^s} = -\frac{b^{1-s}}{1-s}$,
+-- $$\sum_{n\leq b} \frac{1}{n^s} = \zeta(s) + \frac{b^{1-s}}{1-s} + s \int_b^\infty \left(\{y\}-\frac{1}{2}\right) \frac{dy}{y^{s+1}};$$
+-- by analytic continuation, this equation actually holds for $\Re s>0$. Hence, for our $s$,
+-- $$\sum_{n\leq a} \frac{1}{n^s} = -\sum_{a < n\leq b} \frac{1}{n^s} + \zeta(s) + \frac{b^{1-s}}{1-s} + O\left(\frac{|s|}{\sigma b^\sigma}\right).$$
+-- Let $f(y) = 1_{[a,b]}(y)/y^s$. Then, by Poisson summation,
+-- $$\sum_{a < n\leq b} \frac{1}{n^s} = \sum_{n\in \mathbb{Z}} f(n) = \lim_{N\to \infty} \sum_{n=-N}^N \widehat{f}(n).$$
+-- (See, e.g., Thm.~D.3 of MR2378655; notice $f$ is continuous at every integer.) Clearly
+-- $$\widehat{f}(0) = \int_{\mathbb{R}} f(y) dy = \int_a^b \frac{dy}{y^s} = \frac{b^{1-s}-a^{1-s}}{1-s},$$
+-- and so
+-- $$\sum_{n\leq a} \frac{1}{n^s} = \zeta(s) + \frac{a^{1-s}}{1-s} - \lim_{N\to \infty} \sum_{n=1}^N (\widehat{f}(n) + \widehat{f}(-n)) + O\left(\frac{|s|}{\sigma b^\sigma}\right).$$
+-- Applying Lemma \ref{lem:applem} and multiplying by $2$ (since $e(-n t)+e(n t) = 2 \cos 2\pi n t$), we see that
+-- $$\begin{aligned}\widehat{f}(n) + \widehat{f}(-n) &=
+--  \frac{(-1)^{n+1} (a^{-s} +O^*\left(\frac{a}{b^{\sigma+1}}\right))}{2\pi i} \frac{2\rho}{n^2 - \rho^2} \\
+--  &+ \frac{a^{-\sigma-1}}{2\pi^2} O^*\left(\frac{\sigma}{(n-\rho)^2} + \frac{\sigma}{(n+\rho)^2} + \frac{|\rho|}{(n-\rho)^3} + \frac{|\rho|}{(n+\rho)^3}\right).\end{aligned}$$
+-- (The term involving $b$ really gets divided by $n^2-\left(\frac{\tau}{2\pi b}\right)^2$, but that is larger than $n^2 - \rho^2$.) We have Euler's identities $\sum_{n=1}^\infty \frac{(-1)^{n+1} \cdot 2\rho}{n^2-\rho^2} = \frac{\pi}{\sin \pi \rho} - \frac{1}{\rho}$ and $\sum_{n=-\infty}^\infty \frac{1}{(n-\rho)^2} = \frac{\pi^2}{\sin^2 \pi \rho}$. Moreover, $$\begin{aligned}\sum_{n=1}^\infty \left(\frac{1}{(n-\rho)^3} + \frac{1}{(n+\rho)^3}\right) &=\frac{1}{(1-|\rho|)^3} + \sum_{n=1}^\infty \left(\frac{1}{(n+1-|\rho|)^3} + \frac{1}{(n+|\rho|)^3}\right)\\ &\leq \frac{1}{(1-|\rho|)^3} + 2\zeta(3)-1,\end{aligned}$$
+-- since $\frac{1}{(n+1-t)^3} + \frac{1}{(n+t)^3}$ reaches its maximum on $[0,1]$ at $t=0,1$. Thus, since $|\rho|<1$,
+-- $$\begin{aligned}\lim_{N\to \infty} \sum_{n=1}^N (\widehat{f}(n) + \widehat{f}(-n)) &= \frac{a^{-s}+O(b^{-\sigma-1})}{2 i} \left(\frac{1}{\sin \pi \rho} - \frac{1}{\pi \rho}\right) + O^*\left(\frac{C_{\sigma,\rho}}{a^{\sigma+1}}\right),\end{aligned}$$
+-- where $C_{\sigma,\rho}= \frac{\sigma}{2} \left(\frac{1}{\sin^2\pi \rho} - \frac{1}{(\pi \rho)^2}\right) + \frac{|\rho|}{2\pi^2} \left(\frac{1}{(1-|\rho|)^3} + 2\zeta(3)-1\right)$. We let $b\to \infty$ and are done.
+--   -/)
+  ]
 theorem prop_2 {s : ℂ} (hs : 0 ≤ s.re) (a : ℝ)
   (ha : a > |s.im| / (2 * π)) (ha_half : ∃ k : ℤ, a = k + 0.5) :
   let ρ : ℝ := s.im / (2 * π * a)
@@ -114,13 +117,15 @@ theorem prop_2 {s : ℂ} (hs : 0 ≤ s.re) (a : ℝ)
         (a ^ (-s.re - 1) : ℝ) * E
     ∧ ‖E‖ ≤ Cσρ / a^(s.re + 1) := by sorry
 
-blueprint_comment /--
- We have not seen the term $c_\rho a^{-s}$ worked out before in the literature; the factor of $i$ in $c_\rho$ was a surprise. If $\rho=0$ (i.e., $s$ is real), then we read $\frac{1}{\sin \pi \rho} - \frac{1}{\pi \rho} = 0$, $\frac{1}{\sin^2\pi \rho} - \frac{1}{(\pi \rho)^2} = \frac{1}{3}$, by continuity; thus, $c_0=0$ and $C_{\sigma,0} = \frac{\sigma}{6}$.  If $a\geq x$, then $|\rho|\leq 1/2\pi$, and so $|c_\rho|\leq |c_{\pm 1/2\pi}| = 0.04291\dotsc$ and
-$|C_{\sigma,\rho}|\leq |C_{\sigma,\pm 1/2\pi}|\leq 0.176\sigma +0.246$. While $c_\rho$ is optimal, $C_{\sigma,\rho}$ need not be --
-but then that is irrelevant for most applications.
+-- blueprint_comment /--
+--  We have not seen the term $c_\rho a^{-s}$ worked out before in the literature; the factor of $i$ in $c_\rho$ was a surprise. If $\rho=0$ (i.e., $s$ is real), then we read $\frac{1}{\sin \pi \rho} - \frac{1}{\pi \rho} = 0$, $\frac{1}{\sin^2\pi \rho} - \frac{1}{(\pi \rho)^2} = \frac{1}{3}$, by continuity; thus, $c_0=0$ and $C_{\sigma,0} = \frac{\sigma}{6}$.  If $a\geq x$, then $|\rho|\leq 1/2\pi$, and so $|c_\rho|\leq |c_{\pm 1/2\pi}| = 0.04291\dotsc$ and
+-- $|C_{\sigma,\rho}|\leq |C_{\sigma,\pm 1/2\pi}|\leq 0.176\sigma +0.246$. While $c_\rho$ is optimal, $C_{\sigma,\rho}$ need not be --
+-- but then that is irrelevant for most applications.
 
-Thus, the fact that $c_\rho$ is imaginary gives us no contradiction.
--/
+-- Thus, the fact that $c_\rho$ is imaginary gives us no contradiction.
+-- -/
+
+
 
 
 
