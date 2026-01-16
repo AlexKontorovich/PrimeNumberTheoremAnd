f84267d29e551bda632a8e76fa11445573792a54
leibniz-rs <mcipollina@recognitionphysics.org>
2026-01-16 18:17:10 +0100

    Update Domain.lean

--- PrimeNumberTheoremAnd/Mathlib/Analysis/Complex/Domain.lean
+++ PrimeNumberTheoremAnd/Mathlib/Analysis/Complex/Domain.lean
@@ -2,7 +2,7 @@ import Mathlib.Data.Complex.Basic
 
 noncomputable section
 
-namespace RH.RS
+namespace Complex
 
 /-- Right half-plane domain Ω = { s : ℂ | 1/2 < Re s }. -/
 def Ω : Set ℂ := { s : ℂ | (1 / 2 : ℝ) < s.re }
