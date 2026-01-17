690487fafeb900209f8a590b9a5e157941f77b48
leibniz-rs <mcipollina@recognitionphysics.org>
2026-01-17 18:13:17 +0100

    Update Annulus.lean

--- PrimeNumberTheoremAnd/Mathlib/Analysis/FromCarleson/Annulus.lean
+++ PrimeNumberTheoremAnd/Mathlib/Analysis/FromCarleson/Annulus.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: James Sundstrom
 -/
 import Mathlib.MeasureTheory.Constructions.BorelSpace.Metric
-import Carleson.ToMathlib.Interval
+import PrimeNUmberTheoremAnd.Mathlib.FromCarleson.Interval
 
 /-!
 # Annulus
