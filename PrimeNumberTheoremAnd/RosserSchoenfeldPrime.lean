230eaa79e3cda7df5e8640eb60dddcfa6f87618b
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-01-18 00:36:47 -0500

    Update RosserSchoenfeldPrime.lean

--- PrimeNumberTheoremAnd/RosserSchoenfeldPrime.lean
+++ PrimeNumberTheoremAnd/RosserSchoenfeldPrime.lean
@@ -12,7 +12,8 @@ In this section we formalize the prime number bounds of Rosser and Schoenfeld \c
 
 namespace RS_prime
 
-open Chebyshev Finset Nat Real
+open scoped Topology
+open Chebyshev Finset Nat Real MeasureTheory
 
 lemma Chebyshev.theta_pos {y : ℝ} (hy : 2 ≤ y) : 0 < θ y := by
   refine sum_pos (fun n hn ↦ log_pos ?_) ⟨2, ?_⟩
@@ -66,7 +67,7 @@ theorem pre_413 {f : ℝ → ℝ} (hf : ContinuousOn f (Set.Ici 2)) {x : ℝ} (h
   (proof := /-- Follows from Sublemma \ref{rs-pre-413} and integration by parts. -/)
   (latexEnv := "sublemma")
   (discussion := 650)]
-theorem eq_413 {f : ℝ → ℝ} (hf : DifferentiableOn ℝ f (Set.Ici 2)) {x : ℝ} (hx : 2 ≤ x) :
+theorem eq_413 {f : ℝ → ℝ} {x : ℝ} (hx : 2 ≤ x) (hf : DifferentiableOn ℝ f (Set.Icc 2 x)) :
     ∑ p ∈ filter Prime (Iic ⌊x⌋₊), f p = f x * θ x / log x -
       ∫ y in 2..x, θ y * deriv (fun t ↦ f t / log t) y := by
   sorry
@@ -80,13 +81,55 @@ theorem eq_413 {f : ℝ → ℝ} (hf : DifferentiableOn ℝ f (Set.Ici 2)) {x : 
   (proof := /-- Follows from Sublemma \ref{rs-413} and integration by parts. -/)
   (latexEnv := "sublemma")
   (discussion := 600)]
-theorem eq_414 {f : ℝ → ℝ} (hf : DifferentiableOn ℝ f (Set.Ici 2)) {x : ℝ} (hx : 2 ≤ x) :
+theorem eq_414 {f : ℝ → ℝ} {x : ℝ} (hx : 2 ≤ x) (hf : DifferentiableOn ℝ f (Set.Icc 2 x))
+    (hd : IntervalIntegrable (fun t => deriv (fun s ↦ f s / log s) t) volume 2 x) :
     ∑ p ∈ filter Prime (Iic ⌊x⌋₊), f p =
-      ∫ y in 2..x, f y / log y + 2 * f 2 / Real.log 2 +
+      (∫ y in 2..x, f y / log y) + 2 * f 2 / Real.log 2 +
       f x * (θ x - x) / log x -
       ∫ y in 2..x, (θ y - y) * deriv (fun s ↦ f s / log s) y := by
-  sorry
-
+    let h := Set.uIcc_of_le hx
+    calc
+    _ = f x * (θ x - x) / log x + x * f x / log x -
+      (∫ y in 2..x, (θ y - y) * deriv (fun t ↦ f t / log t) y) -
+      ∫ y in 2..x, y * deriv (fun t ↦ f t / log t) y := by
+      rw [eq_413 hx hf, ← tsub_add_eq_tsub_tsub, ← intervalIntegral.integral_add _
+        (IntervalIntegrable.continuousOn_mul hd (by fun_prop))]
+      · ring_nf
+      · sorry
+    _ = f x * (θ x - x) / log x +
+      ((∫ y in 2..x, 1 * (f y / log y)+ y * derivWithin (fun t ↦ f t / log t) (Set.uIcc 2 x) y) +
+      2 * f 2 / log (2 : ℝ)) -
+      (∫ y in 2..x, (θ y - y) * deriv (fun t ↦ f t / log t) y) -
+      ∫ y in 2..x, y * deriv (fun t ↦ f t / log t) y := by
+      rw [← sub_add_cancel (x * f x / log x) (2 * f 2 / log (2 : ℝ)),
+        intervalIntegral.integral_deriv_mul_eq_sub_of_hasDerivWithinAt, mul_div, mul_div]
+      · intro y _; exact (hasDerivAt_id' y).hasDerivWithinAt
+      · refine fun y hy => DifferentiableWithinAt.hasDerivWithinAt (h ▸
+          DifferentiableWithinAt.fun_div (hf y (h ▸ hy)) ?_ ?_)
+        · exact (differentiableAt_log (by simp_all; linarith)).differentiableWithinAt
+        · linarith [Real.log_pos (by simp_all; linarith)]
+      · exact intervalIntegral.intervalIntegrable_const
+      · have : Set.Ioo 2 x ∈ ae (volume.restrict (Set.Ioc 2 x)) := by
+          by_cases hp : 2 < x
+          · rw [mem_ae_iff, Measure.restrict_apply' measurableSet_Ioc, ← Set.diff_eq_compl_inter,
+              Set.Ioc_diff_Ioo_same hp, volume_singleton]
+          · simp_all
+        refine hd.congr_ae ((Set.uIoc_of_le hx) ▸ ?_)
+        filter_upwards [this] with y hy
+        have : Set.Icc 2 x ∈ 𝓝 y := mem_nhds_iff.2
+          ⟨Set.Ioo 2 x, Set.Ioo_subset_Icc_self, ⟨isOpen_Ioo, hy⟩⟩
+        refine (DifferentiableAt.derivWithin ?_ (uniqueDiffWithinAt_of_mem_nhds (h ▸ this))).symm
+        refine DifferentiableAt.fun_div ?_ (differentiableAt_log (by simp_all; linarith)) ?_
+        · refine DifferentiableWithinAt.differentiableAt (hf y (Set.Ioo_subset_Icc_self hy)) this
+        · linarith [Real.log_pos (by simp_all; linarith)]
+    _ = f x * (θ x - x) / log x +
+      ((∫ y in 2..x, f y / log y) + (∫ y in 2..x, y * deriv (fun t ↦ f t / log t) y) +
+      2 * f 2 / log (2 : ℝ)) -
+      (∫ y in 2..x, (θ y - y) * deriv (fun t ↦ f t / log t) y) -
+      ∫ y in 2..x, y * deriv (fun t ↦ f t / log t) y := by sorry
+    _ = (∫ y in 2..x, f y / log y) + 2 * f 2 / Real.log 2 +
+      f x * (θ x - x) / log x -
+      ∫ y in 2..x, (θ y - y) * deriv (fun s ↦ f s / log s) y := by ring
 
 @[blueprint
   "rs-416"
@@ -136,9 +179,9 @@ theorem eq_417 {x : ℝ} (hx : 2 ≤ x) :
 theorem eq_418 {x : ℝ} (hx : 2 ≤ x) :
     ∑ p ∈ filter Prime (Iic ⌊x⌋₊), 1 / (p : ℝ) = θ x / (x * log x) +
       ∫ y in 2..x, θ y * (1 + log y) / (y ^ 2 * log y ^ 2) := by
-  have : DifferentiableOn ℝ (fun x : ℝ ↦ 1 / x) (Set.Ici 2) :=
-    fun x hx => by simpa [one_div] using differentiableWithinAt_inv (by grind) (Set.Ici 2)
-  rw [eq_413 (f := fun x => 1 / x) this hx, mul_comm_div, one_mul, div_div, sub_eq_add_neg,
+  have : DifferentiableOn ℝ (fun y : ℝ ↦ 1 / y) (Set.Icc 2 x) :=
+    fun y hy => by simpa [one_div] using differentiableWithinAt_inv (by grind) (Set.Icc 2 x)
+  rw [eq_413 (f := fun x => 1 / x) hx this, mul_comm_div, one_mul, div_div, sub_eq_add_neg,
     ← intervalIntegral.integral_neg, add_left_cancel_iff]
   refine intervalIntegral.integral_congr fun y hy => ?_
   have hy := Set.uIcc_of_le hx ▸ hy
