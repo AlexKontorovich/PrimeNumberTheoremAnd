2601d6b6fd4a6102b6c18d10edf6fb42e7ce985c
Tom <39548689+Tomodovodoo@users.noreply.github.com>
2026-01-20 15:28:24 +0100

    feat(Wiener): golf proof
    based on @pitmonticone 's revision
    
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- PrimeNumberTheoremAnd/Wiener.lean
+++ PrimeNumberTheoremAnd/Wiener.lean
@@ -3991,20 +3991,15 @@ theorem WeakPNT_AP_prelim {q : ℕ} {a : ℕ} (hq : q ≥ 1) (ha : Nat.Coprime a
     ∃ G: ℂ → ℂ, (ContinuousOn G {s | 1 ≤ s.re}) ∧
     (Set.EqOn G (fun s ↦ LSeries (fun n ↦ if n % q = a then Λ n else 0) s - 1 /
       ((Nat.totient q) * (s - 1))) {s | 1 < s.re}) := by
-        have : NeZero q := ⟨by omega⟩;
-        have hG : ∃ G : ℂ → ℂ, ContinuousOn G {s | 1 ≤ s.re} ∧ Set.EqOn G (fun s => LSeries (fun n => if (n : ZMod q) = a then Λ n else 0) s - (q.totient : ℂ)⁻¹ / (s - 1)) {s | 1 < s.re} := by
-          have hG : ∃ G : ℂ → ℂ, ContinuousOn G {s | 1 ≤ s.re} ∧ Set.EqOn G (fun s => LSeries (fun n => if (n : ZMod q) = a then Λ n else 0) s - (q.totient : ℂ)⁻¹ / (s - 1)) {s | 1 < s.re} := by
-            have := ArithmeticFunction.vonMangoldt.continuousOn_LFunctionResidueClassAux (q := q) (a := a)
-            use ArithmeticFunction.vonMangoldt.LFunctionResidueClassAux (a : ZMod q)
-            have := ArithmeticFunction.vonMangoldt.eqOn_LFunctionResidueClassAux (q := q) (a := a)
-            simp_all +decide only [ge_iff_le, vonMangoldt.residueClass, ZMod.natCast_eq_natCast_iff', true_and]
-            convert this _ using 4
-            · aesop
-            exact (ZMod.isUnit_iff_coprime a q).mpr ha
-          exact hG
-        convert hG using 6
-        · simp +decide [ ZMod.natCast_eq_natCast_iff', Nat.mod_eq_of_lt ha' ]
-        · rw [ inv_eq_one_div, div_div ]
+  have : NeZero q := NeZero.of_pos hq
+  have hG : ∃ G : ℂ → ℂ, ContinuousOn G {s | 1 ≤ s.re} ∧ Set.EqOn G
+      (fun s ↦ LSeries (fun n ↦ if (n : ZMod q) = a then Λ n else 0) s - (q.totient : ℂ)⁻¹ / (s - 1)) {s | 1 < s.re} := by
+    use vonMangoldt.LFunctionResidueClassAux (a : ZMod q), vonMangoldt.continuousOn_LFunctionResidueClassAux (q := q) (a := a)
+    have := vonMangoldt.eqOn_LFunctionResidueClassAux ((ZMod.isUnit_iff_coprime a q).mpr ha)
+    convert this using 6; split <;> simp_all
+  convert hG using 6
+  · simp [ZMod.natCast_eq_natCast_iff', Nat.mod_eq_of_lt ha']
+  · rw [inv_eq_one_div, div_div]
 
 /-- The von Mangoldt function divided by `n ^ s` is summable for `s > 1`. -/
 lemma summable_vonMangoldt_div_rpow {s : ℝ} (hs : 1 < s) : Summable (fun n ↦ Λ n / n ^ s) := by
