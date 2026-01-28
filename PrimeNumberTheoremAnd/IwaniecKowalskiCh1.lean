7468bd23dbdfe923d5fc635fe305f6c40c16eca0
Alex Kontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2026-01-28 15:08:36 -0500

    feat[IK]: GenArithFunction (#818)
    * pq_ratio_ge done
    
    * inv_n_add_sqrt_ge done
    
    * fix
    
    * fixes
    
    * lemma_abadeulmac almost done
    
    * lemma_abadeulmac done
    
    * put back `lemma_abadeulmac`
    
    * lemma_abadeulmac is done
    
    * removing aristotle file
    
    * blueprint fix
    
    * fix lemma_abadeuleulmit1
    
    * undo
    
    * draft
    
    * hmm
    
    * progress
    
    * another attempt
    
    * take 2
    
    * progress
    
    * refactor score_bound theorem: introduce new variables for prime sums and comment out unused code
    
    * feat(IwaniecKowalski): add chapter 1 blueprint and related definitions
    
    * refactor(score_bound): simplify theorem by removing unused calculations and comments
    
    * feat(IwaniecKowalski): introduce GenArithFunction and update IsAdditive definition
    
    * refactor(IwaniecKowalski): change definition of GenArithFunction to use abbrev for clarity
    
    ---------
    
    Co-authored-by: teorth <tao@math.ucla.edu>

--- PrimeNumberTheoremAnd/IwaniecKowalskiCh1.lean
+++ PrimeNumberTheoremAnd/IwaniecKowalskiCh1.lean
@@ -8,13 +8,32 @@ blueprint_comment /--
 \section{Blueprint for Iwaniec-Kowalski Chapter 1}
 -/
 
-variable {R : Type*}
+blueprint_comment /--
+We will want to treat very general additive and multiplicative functions, and set up the API for them.
+So if we just use natural numbers for the domain, we will
+eventually suffer when we, e.g., want to consider functions defined on ideals in number fields.
+Thus we set up the definitions in a more general way.
 
+Therefore we introduce a generalized `ArithmeticFunction` called `GenArithFunction`.
+-/
+
+@[blueprint
+  "GenArithFunction"
+  (statement := /-- Generalized arithmetic function on a type with multiplication and one. -/)]
+abbrev GenArithFunction (N R : Type*) [Zero N] [Zero R] :=
+  ZeroHom N R
+
+variable {N R : Type*}
+
+/-
+We need to add an instance or something so that
+we can automatically coerce `GenArithFunction N R` to a function `N → R`.
+-/
 @[blueprint
   "IsAdditive"
   (statement := /-- Additive function. -/)]
-def IsAdditive [AddZeroClass R] (f : ArithmeticFunction R) : Prop :=
-  ∀ {m n : ℕ}, m.Coprime n → f (m * n) = f m + f n
+def IsAdditive [MonoidWithZero N] [AddZeroClass R] (f : GenArithFunction N R) : Prop :=
+  ∀ {m n : N}, IsRelPrime m n → f.toFun (m * n) = f.toFun m + f.toFun n
 
 @[blueprint
   "IsCompletelyAdditive"
