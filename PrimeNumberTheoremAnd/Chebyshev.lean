144850002bf549913f7d8ce8c22cb90898edfb28
teorth <tao@math.ucla.edu>
2026-02-01 08:06:07 -0800

    relax continuity to measurability in second_fourier (#845)
    * relax continuity to measurability in second_fourier
    
    * simplify proof

--- PrimeNumberTheoremAnd/Chebyshev.lean
+++ PrimeNumberTheoremAnd/Chebyshev.lean
@@ -24,7 +24,8 @@ noncomputable def T (x : ℝ) : ℝ :=
   (proof := /-- Upper bound $\log n$ by $\int_n^{n+1} \log t\ dt$ for $n < x-1$ and by $\log x$ for $x-1 < n \leq x$ to bound
   $$T(x) \leq \int_1^x \log t\ dt + \log x$$
   giving the claim. -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 831)]
 theorem T.le (x : ℝ) (hx : 1 ≤ x) : T x ≤ x * log x - x + 1 + log x := by
   sorry
 
@@ -35,7 +36,8 @@ theorem T.le (x : ℝ) (hx : 1 ≤ x) : T x ≤ x * log x - x + 1 + log x := by
   (proof := /-- Drop the $n=1$ term. Lower bound $\log n$ by $\int_{n-1}^{n} \log t\ dt$ for $2 \leq n < x$ to bound
   $$T(x) \geq \int_1^{\lfloor x \rfloor} \log t\ dt \geq \int_1^x \log t\ dt - \log x $$
   giving the claim. -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 832)]
 theorem T.ge (x : ℝ) (hx : 1 ≤ x) : T x ≥ x * log x - x + 1 - log x := by
   sorry
 
@@ -44,7 +46,8 @@ theorem T.ge (x : ℝ) (hx : 1 ≤ x) : T x ≥ x * log x - x + 1 - log x := by
   (title := "Relating $T$ and von Mangoldt")
   (statement := /-- For $x \geq 0$, we have $T(x) = \sum_{n \leq x} \Lambda(n) \lfloor x/n \rfloor$. -/)
   (proof := /-- This follows from the identity $\log n = \sum_{d|n} \Lambda(d)$ and rearranging sums. -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 833)]
 theorem T.eq_sum_Lambda (x : ℝ) (hx : 0 ≤ x) : T x = ∑ n ∈ Finset.Icc 1 ⌊x⌋₊, Λ n * ⌊x / n⌋₊ := by
   sorry
 
@@ -59,7 +62,8 @@ noncomputable def E (ν : ℕ →₀ ℝ) (x : ℝ) : ℝ := ν.sum (fun m w ↦
   (title := "Relating a weighted sum of $T$ to an $E$-weighted sum of von Mangoldt")
   (statement := /-- If $\nu : \N \to \R$ is finitely supported, then
 $$ \sum_m \nu(m) T(x/m) = \sum_{n \leq x} E(x/n) \Lambda(n).$$ -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 834)]
 theorem T.weighted_eq_sum (ν : ℕ →₀ ℝ) (x : ℝ) : ν.sum (fun m w ↦ w * T (x/m)) = ∑ n ∈ Finset.Icc 1 ⌊x⌋₊, Λ n * E ν (x/n) := by sorry
 
 open Finsupp in
@@ -84,7 +88,8 @@ theorem nu_sum_div_eq_zero : ν.sum (fun n w ↦ w / n) = 0 := by
   (title := "$E$ initially constant")
   (statement := /-- One has $E(x)=1$ for $1 \leq x \leq 6$. -/)
   (proof := /-- This follows from direct computation. -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 835)]
 theorem E_nu_eq_one (x : ℝ) (hx : x ∈ Set.Icc 1 6) : E ν x = 1 := by sorry
 
 @[blueprint
@@ -115,7 +120,8 @@ theorem E_nu_period (x : ℝ) (hx : x ≥ 0) : E ν (x + 30) = E ν x := by
   (title := "$E$ lies between $0$ and $1$")
   (statement := /-- One has $0 \leq E(x) \leq 1$ for all $x \geq 0$. -/)
   (proof := /-- This follows from direct computation for $0 \leq x < 30$, and then by periodicity for larger $x$. -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 836)]
 theorem E_nu_bound (x : ℝ) (hx : x ≥ 0) : 0 ≤ E ν x ∧ E ν x ≤ 1 := by sorry
 
 @[blueprint
@@ -127,17 +133,19 @@ noncomputable def U (x : ℝ) : ℝ := ν.sum (fun m w ↦ w * T (x/m))
 @[blueprint
   "cheby-psi-lower"
   (title := "Lower bounding $\\psi$ by a weighted sum of $T$")
-  (statement := /-- For any $x > 0$, one has $\psi(x) \geq \sum_m \nu(m) T(x/m)$. -/)
+  (statement := /-- For any $x > 0$, one has $\psi(x) \geq U(x)$. -/)
   (proof := /-- Use Lemma \ref{cheby-T-E} and Lemma \ref{cheby-E-val}. -/)
-  (latexEnv := "proposition")]
+  (latexEnv := "proposition")
+  (discussion := 837)]
 theorem psi_ge_weighted (x : ℝ) (hx : x > 0) : ψ x ≥ U x := by sorry
 
 @[blueprint
   "cheby-psi-diff"
   (title := "Upper bounding a difference of $\\psi$ by a weighted sum of $T$")
-  (statement := /-- For any $x > 0$, one has $\psi(x) - \psi(x/6) \leq \sum_m \nu(m) T(x/m)$. -/)
+  (statement := /-- For any $x > 0$, one has $\psi(x) - \psi(x/6) \leq U(x)$. -/)
   (proof := /-- Use Lemma \ref{cheby-T-E}, Lemma \ref{cheby-E-val}, and Lemma \ref{cheby-E-1}. -/)
-  (latexEnv := "proposition")]
+  (latexEnv := "proposition")
+  (discussion := 838)]
 theorem psi_diff_le_weighted (x : ℝ) (hx : x > 0) : ψ x - ψ (x / 6) ≤ U x := by sorry
 
 @[blueprint
@@ -150,7 +158,8 @@ noncomputable def a : ℝ := - ν.sum (fun m w ↦ w * log m / m)
   "a-val"
   (title := "Numerical value of $a$")
   (statement := /-- We have $0.92129 \leq a \leq 0.92130$. -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 839)]
 theorem a_bound : a ∈ Set.Icc 0.92129 0.92130 := by sorry
 
 @[blueprint
@@ -158,7 +167,8 @@ theorem a_bound : a ∈ Set.Icc 0.92129 0.92130 := by sorry
   (title := "Bounds for $U$")
   (statement := /-- For $x \geq 1$, we have $|U(x) - ax| \leq 5(\log x + 1)$. -/)
   (proof := /-- Use Lemma \ref{cheby-T-upper}, Lemma \ref{cheby-T-lower}, the definition of $a$, and the triangle inequality -/)
-  (latexEnv := "lemma")]
+  (latexEnv := "lemma")
+  (discussion := 840)]
 theorem U_bound (x : ℝ) (hx : 1 ≤ x) : |U x - a * x| ≤ 5 * (log x + 1) := by sorry
 
 @[blueprint
@@ -166,7 +176,8 @@ theorem U_bound (x : ℝ) (hx : 1 ≤ x) : |U x - a * x| ≤ 5 * (log x + 1) := 
   (title := "Lower bound for $\\psi$")
   (statement := /-- For $x \geq 1$, we have $\psi(x) \geq ax - 5(\log x + 1)$. -/)
   (proof := /-- Use Lemma \ref{U-bounds} and Proposition \ref{cheby-psi-lower}.-/)
-  (latexEnv := "theorem")]
+  (latexEnv := "theorem")
+  (discussion := 841)]
 theorem psi_lower (x : ℝ) (hx : 1 ≤ x) : ψ x ≥ a * x - 5 * (log x + 1) := by sorry
 
 @[blueprint
@@ -174,7 +185,8 @@ theorem psi_lower (x : ℝ) (hx : 1 ≤ x) : ψ x ≥ a * x - 5 * (log x + 1) :=
   (title := "Upper bound for $\\psi$ difference")
   (statement := /-- For $x \geq 1$, we have $\psi(x) - \psi(x/6) \leq ax + 5(\log x + 1)$. -/)
   (proof := /-- Use Lemma \ref{U-bounds} and Proposition \ref{cheby-psi-upper}.-/)
-  (latexEnv := "proposition")]
+  (latexEnv := "proposition")
+  (discussion := 842)]
 theorem psi_diff_upper (x : ℝ) (hx : 1 ≤ x) : ψ x - ψ (x / 6) ≤ a * x + 5 * (log x + 1) := by sorry
 
 @[blueprint
@@ -182,7 +194,8 @@ theorem psi_diff_upper (x : ℝ) (hx : 1 ≤ x) : ψ x - ψ (x / 6) ≤ a * x + 
   (title := "Upper bound for $\\psi$")
   (statement := /-- For $x \geq 1$, we have $\psi(x) \leq 6ax/5 + 5(\log 5 / \log 6 + 1)(\log x + 1)$. -/)
   (proof := /-- Iterate Lemma \ref{psi-diff-upper}.-/)
-  (latexEnv := "theorem")]
+  (latexEnv := "theorem")
+  (discussion := 843)]
 theorem psi_upper (x : ℝ) (hx : 1 ≤ x) : ψ x ≤ 6 * a * x / 5 + 5 * (log 5 / log 6 + 1) * (log x + 1) := by sorry
 
 @[blueprint
@@ -190,7 +203,8 @@ theorem psi_upper (x : ℝ) (hx : 1 ≤ x) : ψ x ≤ 6 * a * x / 5 + 5 * (log 5
   (title := "Clean upper bound for $\\psi$")
   (statement := /-- For $x > 0$, we have $\psi(x) \leq 1.11 x$. -/)
   (proof := /-- For $x$ large enough, this follows from Theorem \ref{psi-upper}. For small $x$, this follows from direct computation.-/)
-  (latexEnv := "theorem")]
+  (latexEnv := "theorem")
+  (discussion := 844)]
 theorem psi_upper_clean (x : ℝ) (hx : x > 0) : ψ x ≤ 1.11 * x := by sorry
 
 
