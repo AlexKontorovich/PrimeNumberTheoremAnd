153bcd8c3e00b9d1bb522cba219f8cffffe86ac4
Terence Tao <tao@math.ucla.edu>
2026-02-08 09:23:51 -0800

    discussion numbers

--- PrimeNumberTheoremAnd/eSHP.lean
+++ PrimeNumberTheoremAnd/eSHP.lean
@@ -16,7 +16,8 @@ namespace eSHP
   (statement := /--
   For every pair $(p_k,g_k)$ in Table 8 with $p_k < 30$, $g_k$ is the prime gap $p_{k+1} - p_k$, and all prime gaps preceding this gap are less than $g_k$. -/)
   (proof := /-- Direct computation. -/)
-  (latexEnv := "proposition")]
+  (latexEnv := "proposition")
+  (discussion := 902)]
 theorem table_8_prime_gap_test (p g : ℕ) (h : (p, g) ∈ table_8) (htest : p < 30) : prime_gap_record p g := by
   sorry
 
@@ -31,12 +32,13 @@ theorem table_8_prime_gap (p g : ℕ) (h : (p, g) ∈ table_8) : prime_gap_recor
   sorry
 
 @[blueprint
-  "table-9-prime-gap"
+  "table-9-prime-gap-test"
   (title := "Table 9 prime gaps - unit test")
   (statement := /--
   For every pair $(g,P)$ in Table 9 with $P < 30$, $P$ is the first prime producing the prime gap $g$, and all smaller $g'$ (that are even or $1$) have a smaller first prime. -/)
   (proof := /-- Direct computation. -/)
-  (latexEnv := "proposition")]
+  (latexEnv := "proposition")
+  (discussion := 903)]
 theorem table_9_prime_gap_test (g P : ℕ) (h : (g, P) ∈ table_9) (htest : P < 30) : first_gap_record g P := by
   sorry
 
