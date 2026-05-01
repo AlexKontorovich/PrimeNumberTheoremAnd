e3e928af3cde0d5bbcc3eb7d0071816406584d59
Alastair Irving <29164966+ajirving@users.noreply.github.com>
2026-05-01 17:25:58 +0100

    Initial version of Euler-Maclaurin

--- PrimeNumberTheoremAnd/Mertens.lean
+++ PrimeNumberTheoremAnd/Mertens.lean
@@ -27,6 +27,77 @@ theorem one_eq_o_log_log : (fun _ ↦ (1:ℝ)) =o[atTop] (fun x ↦ log (log x))
 
 end Real
 
+section EulerMaclaurin
+open Finset Interval MeasureTheory
+
+variable {𝕜 : Type*} [RCLike 𝕜] {f : ℝ → 𝕜} {a b : ℝ}
+
+noncomputable def B1 (x : ℝ) : ℝ := x - ⌊x⌋₊ - 1 / 2
+
+@[fun_prop]
+lemma aestronglyMeasurable_B1 : AEStronglyMeasurable B1 := by
+  unfold B1
+  fun_prop
+
+lemma abs_B1_le_half {x : ℝ} (hx : 0 ≤ x) : |B1 x| ≤ 1 / 2 := by
+  unfold B1
+  refine abs_le.mpr ⟨?_, ?_⟩
+  · grind [Nat.floor_le hx]
+  · grind [Nat.lt_succ_floor x]
+
+lemma integral_deriv_mul_add_half (hab : a ≤ b) (h_int : IntervalIntegrable (deriv f) volume a b)
+    (hf_diff : ∀ t ∈ Set.Icc a b, DifferentiableAt ℝ f t) :
+    ∫ t in a..b, (t + 1 / 2) * deriv f t = (b + 1 / 2) * f b - (a + 1 / 2) * f a - ∫ t in a..b, f t := by
+  rw [← Set.uIcc_of_le hab] at hf_diff
+  have : ∀ t ∈ [[a, b]], HasDerivAt (fun (t : ℝ) ↦ ((t + 1 / 2) : 𝕜)) 1 t := by
+    intro t ht
+    simp only [hasDerivAt_add_const_iff]
+    convert ContinuousLinearMap.hasDerivAt (RCLike.ofRealCLM (K := 𝕜)) using 1
+    simp
+  replace hf_diff := fun t ht ↦ (hf_diff t ht).hasDerivAt
+  rw [intervalIntegral.integral_mul_deriv_eq_deriv_mul this hf_diff (by simp) h_int]
+  simp
+
+lemma integral_deriv_mul_floor_add_one (ha : 0 ≤ a) (hab : a ≤ b)
+    (hf_diff : ∀ t ∈ Set.Icc a b, DifferentiableAt ℝ f t) (h_cont : ContinuousOn (deriv f) [[a, b]]) :
+    ∫ t in a..b, deriv f t * (⌊t⌋₊ + 1) = (b + 1 / 2) * f b - (a + 1 / 2) * f a - (∫ t in a..b, f t) - ∫ t in a..b, deriv f t * B1 t := by
+  calc
+  _ = ∫ t in a..b, (deriv f t * (t + 1 / 2) -deriv f t * B1 t) := by
+    congr
+    ext
+    simp only [B1]
+    push_cast
+    ring
+  _ = (∫ t in a..b, deriv f t * (t + 1 / 2)) - ∫ t in a..b, deriv f t * B1 t := by
+    apply intervalIntegral.integral_sub
+    · exact ContinuousOn.intervalIntegrable (by fun_prop)
+    · refine IntervalIntegrable.continuousOn_mul ?_ h_cont
+      rw [intervalIntegrable_iff']
+      apply MeasureTheory.Measure.integrableOn_of_bounded (by simp) (by fun_prop) (M := 1 / 2)
+      filter_upwards [self_mem_ae_restrict (by measurability)] with x hx
+      rw [Set.uIcc_of_le hab, Set.mem_Icc] at hx
+      norm_cast
+      exact abs_B1_le_half (by linarith)
+  _ = _ := by
+    conv => lhs; arg 1; arg 1; ext; rw [mul_comm]
+    rw [integral_deriv_mul_add_half hab h_cont.intervalIntegrable hf_diff]
+
+theorem sum_eq_integral_add_integral_deriv (ha : 0 ≤ a) (hab : a ≤ b)
+    (hf_diff : ∀ t ∈ Set.Icc a b, DifferentiableAt ℝ f t)
+    (h_cont : ContinuousOn (deriv f) [[a, b]]) :
+    ∑ k ∈ Ioc ⌊a⌋₊ ⌊b⌋₊, f k =
+      f a * B1 a - f b * B1 b + (∫ t in a..b, f t) + ∫ t in a..b, deriv f t * B1 t  := by
+  have := sum_mul_eq_sub_sub_integral_mul (fun _ ↦ 1) ha hab hf_diff (Set.uIcc_of_le hab ▸ h_cont).integrableOn_Icc
+  simp only [mul_one, sum_const, Nat.card_Icc, tsub_zero, nsmul_eq_mul, Nat.cast_add,
+    Nat.cast_one] at this
+  rw [this, ← intervalIntegral.integral_of_le hab]
+  rw [integral_deriv_mul_floor_add_one ha hab hf_diff h_cont]
+  unfold B1
+  push_cast
+  ring
+
+end EulerMaclaurin
+
 namespace Mertens
 
 blueprint_comment /--
