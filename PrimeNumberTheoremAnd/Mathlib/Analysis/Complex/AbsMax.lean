ee8b7776d6ee92bfbd079efe28354b082c375682
Matteo Cipollina <matteo.cipollina@yahoo.it>
2026-06-07 12:33:19 +0200

    minor cleanup

--- PrimeNumberTheoremAnd/Mathlib/Analysis/Complex/AbsMax.lean
+++ PrimeNumberTheoremAnd/Mathlib/Analysis/Complex/AbsMax.lean
@@ -1,8 +1,3 @@
-/-
-Copyright (c) 2026 Matteo Cipollina. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Matteo Cipollina
--/
 module
 
 public import Mathlib.Analysis.Complex.AbsMax
